7c4afa372338a9c15d275681496027c3990ec3d1
Fabian <fabian@energyaccessexplorer.org>
2019-12-20 15:49:15 +0100

    regrouped categories json's

--- sql/categories.sql
+++ sql/categories.sql
@@ -4,8 +4,11 @@ create table if not exists
 	, name varchar(32) unique not null check (name ~ '^[a-z][a-z0-9\-]+$')
 	, name_long varchar(64) not null
 	, unit varchar(32)
-	, weight smallint default 2
 	, raster jsonb default 'null'
 	, vectors jsonb default 'null'
+	, csv jsonb default 'null'
+	, analysis jsonb default 'null'
+	, controls jsonb default 'null' -- fix this default
+	, configuration jsonb default '{"path": [], "invert": []}'
 	, metadata jsonb default '{"why": null}'
 	);
