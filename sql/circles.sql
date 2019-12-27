01ddf11179dc24b460ca4600de3da645624c00cd
Fabian <fabian@energyaccessexplorer.org>
2019-12-26 21:40:04 -0600

    tighter members/circles

--- sql/circles.sql
+++ sql/circles.sql
@@ -1,14 +1,30 @@
-create function circles_create(text)
-returns boolean as $$ begin
+create or replace function circles_create(name, name)
+returns boolean as $$
+declare
+	is_member boolean;
+begin
+	select 'member' in
+		(select a.rolname from pg_authid a WHERE pg_has_role($2, a.oid, 'member'))
+	into is_member;
+
+	if not is_member then
+		raise exception 'OH! HELL NAW!';
+	end if;
+
 	execute format('
 		create role %1$s nologin;
-		grant all on all tables in schema public to %1$s;
-	', $1);
+		grant %2$s to %1$s;
+	', $1, $2);
 
 	return true;
 end $$ language plpgsql;
 
-create function circles_delete(n text)
+create function circles_create(name)
+returns boolean as $$ begin
+	return circles_create($1, 'member');
+end $$ language plpgsql;
+
+create function circles_delete(name)
 returns boolean as $$ begin
 	execute format('
 		revoke all on all tables in schema public from %1$s;
--- sql/db.sql
+++ sql/db.sql
@@ -6,11 +6,6 @@ create role root nologin;
 create role admin nologin;
 create role master nologin;
 create role guest nologin;
-
-grant select on all tables in schema public to admin;
-grant select on all tables in schema public to guest;
-
-grant admin to master;
-grant admin to root;
+create role member nologin;
 
 create domain epiphet as name check (value ~ '^[a-z][a-z0-9\-]+$');
