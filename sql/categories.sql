ce3261f7b651a1dd4952ce00ab789e6975046872
Fabian <fabian@energyaccessexplorer.org>
2020-01-09 14:38:43 -0600

    categories.timeline

--- sql/categories.sql
+++ sql/categories.sql
@@ -8,6 +8,7 @@ create table if not exists
 	, vectors jsonb default 'null'
 	, csv jsonb default 'null'
 	, analysis jsonb default 'null'
+	, timeline jsonb default 'null'
 	, controls jsonb default 'null' -- fix this default
 	, configuration jsonb default '{"path": [], "invert": []}'
 	, metadata jsonb default '{"why": null}'
