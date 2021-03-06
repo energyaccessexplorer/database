5f913e132657f2e852463adfbf284108249f18eb
Fabian <eae@carajo.no>
2021-03-06 14:51:11 -0600

    drop files.test

--- files.sql
+++ files.sql
@@ -2,7 +2,6 @@ create table files (
 	  id uuid primary key default gen_random_uuid()
 	, label varchar(32) not null default current_timestamp::text
 	, configuration jsonb default 'null'
-	, test boolean default true
 	, endpoint text unique not null
 	, comment text not null
 	, created date default current_date
@@ -10,16 +9,6 @@ create table files (
 	, updated timestamp with time zone default current_timestamp
 	, updated_by varchar(64)
 	);
-
-create function files_before_delete()
-returns trigger
-language plpgsql immutable as $$ begin
-	if not old.test then
-		raise exception 'You cannot delete non-test files! This has to be done by an database admin by disabling this trigger.';
-	end if;
-
-	return old;
-end $$;
 
 create trigger before_any_create
 	before insert on files
@@ -30,8 +19,3 @@ create trigger before_any_update
 	before update on files
 	for each row
 	execute procedure before_any_update();
-
-create trigger files_before_delete
-	before delete on files
-	for each row
-	execute procedure files_before_delete();
