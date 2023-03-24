ae4d531e0aedeed72536614c8954266a8868a71e
Fabian <eae@carajo.no>
2023-03-24 21:35:21 +0100

    database.mk update

--- database.mk
+++ database.mk
@@ -8,74 +8,65 @@ dump = dumps/latest-data
 .endif
 
 dblist:
-	@psql ${DB} \
+	@ psql ${DB} \
 		--pset="pager=off" \
 		--command="\dt[+]"
 
 dbconsole:
-	PAGER="less -S" psql ${DB}
+	@ PAGER="less -S" psql ${DB}
 
 dbdumpschema:
-	@pg_dump ${DB} \
+	@ pg_dump ${DB} \
 		--verbose \
 		--format=p \
 		--no-owner \
-		--schema-only > dumps/${DUMP}-schema
+		--schema-only \
+		> dumps/${DUMP}-schema
 
-	@(cd dumps && ln -sf ${DUMP}-schema latest-schema)
+	@ (cd dumps && ln -sf ${DUMP}-schema latest-schema)
 
 dbdumpdata:
-	@pg_dump ${DB} \
+	@ pg_dump ${DB} \
 		--verbose \
 		--format=p \
 		--no-owner \
-		--data-only > dumps/${DUMP}-data
+		--data-only \
+		> dumps/${DUMP}-data
 
-	@(cd dumps && ln -sf ${DUMP}-data latest-data)
+	@ (cd dumps && ln -sf ${DUMP}-data latest-data)
 
 dbdrop:
-	psql ${PG}/template1 \
-		--quiet \
-		--command="update pg_database set datallowconn = 'false' where datname = '${DB_NAME}';"
-
-	psql ${PG}/template1 \
-		--quiet \
-		--command="select pg_terminate_backend(pid) from pg_stat_activity where datname = '${DB_NAME}';" >/dev/null
-
-	psql ${PG}/template1 \
-		--quiet \
-		--command="drop database ${DB_NAME};"
+	@ psql ${PG}/template1 --quiet \
+		--command "update pg_database set datallowconn = 'false' where datname = '${DB_NAME}';" \
+		--command "select pg_terminate_backend(pid) from pg_stat_activity where datname = '${DB_NAME}';" \
+		--command "drop database ${DB_NAME};" \
+		> /dev/null
 
 dbcreate:
-	psql ${PG}/template1 \
-		--quiet \
-		--command="create database ${DB_NAME};"
+	@ psql ${PG}/template1 --quiet --command "create database ${DB_NAME};"
 
 dbbuild:
-	@for file in ${SQL_FILES}; do \
+	@ for file in ${SQL_FILES}; do \
 		psql ${DB} \
 			--set="ON_ERROR_STOP=on" \
-			--file="$$file.sql" \
-			>/dev/null; \
+			--file="$$file.sql" >/dev/null; \
 	done
 
 dbrestore:
-	@psql ${DB} \
+	@ psql ${DB} \
 		--set="ON_ERROR_STOP=on" \
 		--pset="tuples_only=on" \
 		--command="set session_replication_role = replica;" \
 		--file="${dump}"
 
-	@echo "Restored from dumpfile: ${dump}"
+	@ echo "Restored from dumpfile: ${dump}"
+
+dbreload:
+	@ psql ${DB} --quiet --command "NOTIFY pgrst, 'reload schema';"
 
 .if ${env} == "production"
 dbrebuild:
-	@echo "PRODUCTION. Do it by hand: dbdrop dbcreate dbbuild dbrestore"
+	@ echo "PRODUCTION. Do it by hand: dbdrop dbcreate dbbuild dbrestore dbreload"
 .else
-dbrebuild: dbdrop dbcreate dbbuild dbrestore
+dbrebuild: dbdrop dbcreate dbbuild dbrestore dbreload
 .endif
-
-dbsnippet:
-	@psql ${DB} \
-		--pset="pager=off" \
-		--file="${snippet}"
