9dd222d42f4632cd37d287fc20b09a0bdbdbb054
Fabian <eae@carajo.no>
2023-03-24 22:53:34 +0100

    housekeeping

--- sessions_rls.sql
+++ sessions_rls.sql
@@ -1,7 +1,7 @@
 alter table sessions enable row level security;
 
 create policy root on sessions
-	using (current_user_root());
+	using (current_role in ('director', 'root'));
 
 create policy owner on sessions
 	using (user_id = current_user_uuid());
