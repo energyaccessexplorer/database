9e0a89ff62b519b27e2baa935e1c21272b023510
Fabian <eae@carajo.no>
2024-01-14 17:00:32 +0100

    more columns for sessions

--- sessions.sql
+++ sessions.sql
@@ -1,5 +1,9 @@
 create table sessions (
 	  time bigint primary key
-	, user_id uuid not null
 	, geography_id uuid references geographies (id)
+	, user_id uuid not null
+	, env environments
+	, check (env is null or array_position(enum_range(null::environments), env) > 0)
+	, title text
+	, unique(title, user_id)
 	);
