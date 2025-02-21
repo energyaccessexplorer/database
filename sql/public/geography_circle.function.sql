346a89094f5ba881b05a03e504ea7863310175d5
Fabian <eae@carajo.no>
2025-02-21 17:19:35 +0100

    replace/drop overloaded functions

--- sql/public/geography_circle.function.sql
+++ sql/public/geography_circle.function.sql
@@ -1,15 +1,4 @@
 --
--- Name: geography_circle(uuid); Type: FUNCTION; Schema: public; Owner: -
---
-
-CREATE FUNCTION public.geography_circle(uuid) RETURNS public.epiphet
-    LANGUAGE sql IMMUTABLE
-    AS $_$
-	select circle from public.geographies where id = $1;
-$_$;
-
-
---
 -- Name: geography_circle(public.datasets); Type: FUNCTION; Schema: public; Owner: -
 --
 
