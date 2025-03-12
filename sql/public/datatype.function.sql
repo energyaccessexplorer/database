75964a06b1263b1baa2c631998ba6ff5c9ccc54e
Fabian <eae@carajo.no>
2025-03-12 07:11:38 +0100

    dropped datasets.datatype

--- sql/public/datatype.function.sql
+++ sql/public/datatype.function.sql
@@ -1,24 +1,40 @@
 --
--- Name: datatype(public.datasets); Type: FUNCTION; Schema: public; Owner: -
+-- Name: datatype(public.categories); Type: FUNCTION; Schema: public; Owner: -
 --
 
-CREATE FUNCTION public.datatype(public.datasets) RETURNS public.epiphet
+CREATE FUNCTION public.datatype(c public.categories) RETURNS public.epiphet
     LANGUAGE plpgsql IMMUTABLE
-    AS $_$
+    AS $$
 declare
-	c categories;
 	x epiphet;
+	y epiphet;
 begin
-	select * from categories where id = $1.category_id into c;
-
-	if (not c.mutant) then
-		return category_datatype(c);
+	if (c.name in ('outline', 'boundaries')) then
+		return 'polygons-boundaries';
 	end if;
 
-	select d.datatype from datasets d
-		where ($1.configuration->'mutant_targets'->>0) = any(array[d.category_name, d.name])
-		and d.geography_id = $1.geography_id into x;
+	select (case
+	when (c.vectors <> 'null') then
+		c.vectors->>'shape_type'
+	when (c.raster <> 'null') then
+		'raster'
+	when (c.csv <> 'null') then
+		'table'
+	when (c.mutant) then
+		'mutant'
+	else
+		null
+	end) into x;
 
-	return coalesce(x || '-', '') || 'mutant';
+	select (case
+	when (c.timeline <> 'null') then
+		'timeline'
+	when (x <> 'table' and c.csv <> 'null') then
+		'valued'
+	else
+		null
+	end) into y;
+
+	return x || coalesce('-' || y, '');
 end
-$_$;
+$$;
